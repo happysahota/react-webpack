2faa5175f7a8804b5aa32f9d6876ef46eac02618
Harpreet Singh Sahota <happy.sahota007@gmail.com>
2017-06-11 13:12:54 +0530

    Adding details to Route to access specific article or feature

--- src/js/Routes/features.js
+++ src/js/Routes/features.js
@@ -2,6 +2,8 @@ import React from "react";
 
 export default class Features extends React.Component {
     render(){
+        let {feature} = this.props.params; //es6 fnctionality to pull key out of obj. this will pull 'feature' out of params and assign to 'feature' variable
+        console.log(feature);
         return (
             <h1>Features Route</h1>
         );
--- src/js/main.js
+++ src/js/main.js
@@ -14,7 +14,7 @@ ReactDom.render(
 <Router history={hashHistory}>
     <Route path="/" component={Layout}>
         <IndexRoute component={Main}></IndexRoute>
-        <Route path="features" component={Features}></Route>
+        <Route path="features/:feature" component={Features}></Route>
         <Route path="settings" component={Settings}></Route>
     </Route>
 </Router>
