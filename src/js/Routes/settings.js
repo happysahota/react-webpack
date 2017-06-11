c90191249b3585bd5ea4a461a5b097942881d477
Harpreet Singh Sahota <happy.sahota007@gmail.com>
2017-06-11 13:41:03 +0530

    Inline Styling magic

--- src/js/Routes/settings.js
+++ src/js/Routes/settings.js
@@ -5,9 +5,13 @@ export default class Settings extends React.Component {
     render(){
         const {query} = this.props.location;
         const {name} = query;
+        const queryStyle = {
+            color:'red',
+            marginLeft:'50px'
+        }
         return (
             <div>
-                <h1>Settings Route: {name}</h1>
+                <h1>Settings Route: <span style={queryStyle}>{name}</span></h1>
                 <Link to="settings?name=happy">Query Demo</Link>
             </div>
         );
