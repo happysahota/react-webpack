b779d3c2438db9d78f72a941119663e4c4d3417e
Harpreet Singh Sahota <happy.sahota007@gmail.com>
2017-06-11 13:22:48 +0530

    making route detail optional and demo the internal linking using route detail

--- src/js/Routes/features.js
+++ src/js/Routes/features.js
@@ -1,11 +1,15 @@
 import React from "react";
+import {Link} from "react-router";
 
 export default class Features extends React.Component {
     render(){
         let {feature} = this.props.params; //es6 fnctionality to pull key out of obj. this will pull 'feature' out of params and assign to 'feature' variable
         console.log(feature);
         return (
-            <h1>Features Route</h1>
+            <div>
+                <h1>Features Route {feature}</h1>
+                <Link to="features/feature-details">Go Details</Link>
+            </div>
         );
     }
 }
