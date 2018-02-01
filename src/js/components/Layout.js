7f9a616862f55526ad4d3979e518feef8b5377f4
Sahota <Harpreet.Sahota@kantar.com>
2018-02-01 10:03:35 +0530

    Axios Support Added

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -1,15 +1,22 @@
 import React from "react";
 
+import axios from "axios";
+
 import Header from "./Header";
 import Footer from "./Footer";
 
 export default class Layout extends React.Component {
+
     constructor(){
         super();
         this.state = {
                         title:"Welcome...!",
                         footer:"footer from Layout"
                     };
+                    
+        axios.get("http://api.openweathermap.org/data/2.5/forecast?q=Delhi,us&appid=726faf11f86f63988b94479c15e877c5&units=metric").then(function(data){
+            console.log(data);
+        });
     }
 
     changeHandler(title){
--- src/js/main.js
+++ src/js/main.js
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDom from "react-dom";
 
 import Layout from "./components/Layout";
-import "../scss/style.scss";
 import "bulma";
+import "../scss/style.scss";
 
 var app = document.getElementById("app");
 ReactDom.render(<Layout/>, app);
